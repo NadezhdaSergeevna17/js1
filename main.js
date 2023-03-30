97abcfe73bee989d5049df3551ad9769289caf7d
Nadezhda <sergenadezh@gmail.com>
2023-03-30 19:37:43 +0300

    create: added file

---
+++ main.js
@@ -0,0 +1,28 @@
+let number = Math.floor(15 * Math.random()) + 1; // случайное число от 1 до 15
+console.log(number)
+
+// ограничить кол-во попыток до 3х
+let count = 2;
+
+document.getElementById('check').onclick = function () {
+  if (count > 0) {  // если попыток больше нуля, то выполняется следующее 
+    let usernum = document.getElementById('mynum').value;
+    usernum = parseInt(usernum);
+    let out = document.getElementById('out');
+
+    if (usernum == number) {
+      out.innerHTML = 'Угадали!!!';
+    }
+    else if (usernum > number) {
+      out.innerHTML = 'Слишком много!';
+    }
+    else if (usernum < number) {
+      out.innerHTML = 'Слишком мало!';
+    }
+    document.getElementById('count').innerHTML = 'Осталось попыток ' + count; // выводим сколько осталось попыток
+    count = count - 1; // с каждой попыткой отнимать 1
+  }
+  else {
+    alert('Кончились попытки, вы проиграли');
+  }
+}
